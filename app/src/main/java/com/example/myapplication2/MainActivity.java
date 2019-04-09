7b6b4b87ff444cdb4ec330e9a5d457c552f80850
Thai <ongthai2803@gmail.com>
2019-04-09 23:42:55 +1200

    Last Commit

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.myapplication2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -19,28 +19,36 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setMax(50);
         final Button btnRedo = findViewById(R.id.btnRedo);
         final Button btnUndo = findViewById(R.id.btnUndo);
+        final Button btnClear = findViewById(R.id.btnClear);
 
         btnUndo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(pv.points.size() > 0){
-                    pv.temp.add(pv.points.remove(pv.points.size()-1));
-                    btnUndo.setText(String.valueOf(pv.temp.size()));
-                    btnRedo.setText(String.valueOf(pv.temp.size()));
-                    pv.invalidate();
-                }
+                pv.Undo();
+//                Toast.makeText(MainActivity.this,
+//                        "point1: "+ pv.point1.size() + " point2: " + pv.point2.size(), Toast.LENGTH_SHORT).show();
             }
         });
 
         btnRedo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(pv.temp.size() > 0){
-                    pv.points.add(pv.temp.remove(pv.temp.size()-1));
-                    btnUndo.setText(String.valueOf(pv.temp.size()));
-                    btnRedo.setText(String.valueOf(pv.temp.size()));
-                    pv.invalidate();
-                }
+                pv.Redo();
+//                Toast.makeText(MainActivity.this,
+//                        "point1: " + pv.point1.size() + " point2: " + pv.point2.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pv.point1.clear();
+                pv.point2.clear();
+                pv.p1.clear();
+                pv.p2.clear();
+                pv.undo1 = 0;
+                pv.undo2 = 0;
+                pv.invalidate();
             }
         });
 
--- app/src/main/java/com/example/myapplication2/PaintView.java
+++ app/src/main/java/com/example/myapplication2/PaintView.java
@@ -13,14 +13,18 @@ import java.util.Random;
 
 public class PaintView extends View implements View.OnTouchListener {
 
-    ArrayList <Point> points = new ArrayList<Point>();
-    ArrayList <Point> temp = new ArrayList<>();
-//    ArrayList <Point> redo = new ArrayList<>();
+    ArrayList <Point> point1 = new ArrayList<Point>();
+    ArrayList <Point> point2 = new ArrayList<>();
+    ArrayList <Point> p1 = new ArrayList<>();
+    ArrayList <Point> p2 = new ArrayList<>();
 
     public int shapeSize = 20;
-//    public int undo = 0;
-//    public  int redo = 0;
-//    public int currentIndex = points.size() - undo + redo;
+    public int undo1 = 0;
+    public  int undo2 = 0;
+//    public Random random = new Random();
+    public int color1 = 0;
+    public int color2 = 0;
+
     class Point {
         float x;
         float y;
@@ -49,19 +53,101 @@ public class PaintView extends View implements View.OnTouchListener {
         setOnTouchListener(this);
     }
 
+    public void Undo(){
+        //Point 1
+        //for replace new list when there is still undo value
+        if(p1.size() > 0 && point1.size() > p1.size()){
+            p1.clear();
+            p1.addAll(point1);
+            point1.clear();
+            undo1 += 1;
+
+            for(int i = 0; i < p1.size() - undo1; i++){
+                point1.add(p1.get(i));
+            }
+        }
+        //after first click
+        else if(p1.size() > 0){
+            if(undo1 < p1.size()) undo1 += 1;
+            point1.clear();
+
+            for(int i = 0; i < p1.size() - undo1; i++){
+                point1.add(p1.get(i));
+            }
+        }
+        //for first click
+        else if(p1.size() == 0){
+            undo1 += 1;
+            p1.addAll(point1);
+            point1.clear();
+
+            for(int i = 0; i < p1.size() - undo1; i++){
+                point1.add(p1.get(i));
+            }
+        }
+
+        //Point 2
+        //for replace new list when there is still undo value
+        if(p2.size() > 0 && point2.size() > p2.size()){
+            p2.clear();
+            p2.addAll(point2);
+            point2.clear();
+            undo2 += 1;
+
+            for(int i = 0; i < p2.size() - undo2; i++){
+                point2.add(p2.get(i));
+            }
+        }
+        //after first click
+        else if(p2.size() > 0){
+            if(undo2 < p2.size()) undo2 += 1;
+            point2.clear();
+
+            for(int i = 0; i < p2.size() - undo2; i++){
+                point2.add(p2.get(i));
+            }
+        }
+        //for first click
+        else if(p2.size() == 0){
+            undo2 += 1;
+            p2.addAll(point2);
+            point2.clear();
+
+            for(int i = 0; i < p2.size() - undo2; i++){
+                point2.add(p2.get(i));
+            }
+        }
+        invalidate();
+    }
+
+    public void Redo(){
+        if(p1.size() > point1.size()){
+            point1.add(p1.get(p1.size() - undo1));
+            undo1 -= 1;
+        }
+
+        if(p2.size() > point2.size()){
+            point2.add(p2.get(p2.size() - undo2));
+            undo2 -= 1;
+        }
+
+        invalidate();
+    }
     final Paint paint = new Paint();
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        for(int i = 0; i < points.size(); i++){
-            paint.setColor(points.get(i).colour);
-            canvas.drawCircle(points.get(i).x, points.get(i).y, points.get(i).size, paint);
+        for(int i = 0; i < point1.size(); i++){
+            paint.setColor(point1.get(i).colour);
+            canvas.drawCircle(point1.get(i).x, point1.get(i).y, point1.get(i).size, paint);
+            canvas.drawCircle(50, 50, point1.get(i).size, paint);
         }
 
-        for(int j = 0; j < temp.size(); j++){
-            paint.setColor((temp.get(j)).colour);
-            canvas.drawCircle(temp.get(j).x, temp.get(j).y, temp.get(j).size, paint);
+        for(int j = 0; j < point2.size(); j++){
+            paint.setColor((point2.get(j)).colour);
+            canvas.drawCircle(point2.get(j).x, point2.get(j).y, point2.get(j).size, paint);
+            canvas.drawCircle(200, 50, point1.get(j).size, paint);
         }
 
     }
@@ -69,35 +155,37 @@ public class PaintView extends View implements View.OnTouchListener {
     public boolean onTouch(View v, MotionEvent event)
     {
         Random random = new Random();
+        boolean onHold = false;
 
+        color1 = random.nextInt();
+        color2 = random.nextInt();
         int pointerCount = event.getPointerCount();
 
-        for(int i = 0; i<pointerCount; i++){
+        if(undo1 > 0){
+            undo1 = 0;
+            p1.clear();
+        }
+        if(undo2 > 0){
+            undo2 = 0;
+            p2.clear();
+        }
+
+
+
+        for(int i = 0; i < pointerCount; i++){
             int x = (int) event.getX(i);
             int y = (int) event.getY(i);
             int id = (int) event.getPointerId(i);
 
             if(id == 0){
-                Point p1 = new Point(x,y,shapeSize,random.nextInt());
-                points.add(p1);
+                Point p1 = new Point(x,y,shapeSize,color1);
+                point1.add(p1);
             }
             if(id == 1){
-                Point p2 = new Point(x,y,shapeSize,random.nextInt());
-                temp.add((p2));
+                Point p2 = new Point(x,y,shapeSize,color2);
+                point2.add((p2));
             }
         }
-
-//        Point point = new Point(event.getX(),event.getY(), shapeSize, random.nextInt());
-////        points.add(point);
-//
-//        if(temp.size() != 0) {
-//            points.set(points.size() - temp.size() -1 ,point);
-//            temp.clear();
-//        }
-//        else{
-//            points.add(point);
-//        }
-
         invalidate();
         return true;
     }
